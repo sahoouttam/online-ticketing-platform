bbba0a060fcd364088622682e48aa6ead64e8e66
sahoouttam <uttamsahoo6205@gmail.com>
2024-08-25 19:23:23 +0530

    first commit

---
+++ src/main/java/com/ticketingplatform/otp/OnlineTicketingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ticketingplatform.otp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTicketingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTicketingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticketingplatform/otp/OnlineTicketingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketingplatform.otp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTicketingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
